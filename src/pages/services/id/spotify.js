d05ef9c3821eb9affef41e861fe3823dbb9e77d0
elze <61447653+elzeXD@users.noreply.github.com>
2020-04-06 20:55:58 +0700

    Update js, semoga gak syntax error

--- src/pages/services/id/spotify.js
+++ src/pages/services/id/spotify.js
@@ -20,12 +20,12 @@ function Spotify({ data }) {
     <Layout>
       <SEO
         url={site.siteMetadata.url}
-        title="Jual Akun & Jasa Upgrade Spotify Premium 12 Bulan"
-        description="Tersedia paket 12 bulan! Harga cuma 50k!"
+        title="Jual & Upgrade Akun Spotify Premium"
+        description="Tersedia paket 12 bulan! Akun asli, bukan APK mod! 100 persen amanah!"
       />
       <div className={Styles.container}>
         <h1 className={Styles.title} style={{ marginTop: `2rem` }}>
-          Jual & Upgrade Akun Spotify Premium 12 Bulan
+          Jual & Upgrade Akun Spotify Premium
         </h1>
         <div className={Styles.lang}>
           <Link to="/services/id/spotify" className={Styles.selected}>
@@ -35,28 +35,35 @@ function Spotify({ data }) {
         </div>
         <div className={Styles.content}>
           <p>
-            100% Premium Asli!
-			BUKAN AKUN CURIAN ATAU MOD!
+            Tersedia paket 12 bulan!
           </p>
 		  <p>
-			Bisa dipasang di Android, iOS, PC, dan Smart TV!
-		  </p>
+			Butuh email dan password Spotify anda, kami akan jaga amanah anda, data tidak akan kami salahgunakan!		
+		  </p>	
+  		  <p>
+			Jika mau, bisa akun	dari kami juga, emailnya tinggal diganti!
+		  </p>    		  
 		  <p>
-		    CATATAN PENTING:
-			Untuk akun jadi premium, kami butuh akun Spotify anda, berikut email dan passwordnya.
-			Tenang, kami tidak akan mencuri akun anda.
-			Kami tidak butuh informasi pribadi anda.
-			Kami akan jaga amanah 100%, jadi setelah akun menjadi premium, anda disarankan untuk mengganti password.
-			Jika masih tidak mau, kami akan membuatkan akun baru, anda tinggal ganti saja emailnya.
+			PS. Tidak support akun Facebook
+		  </p>  		 
+		  <p>
+			100% ASLI, BUKAN AKUN CURIAN ATAU MOD!
+			Support Android, iOS, PC, dan Smart TV!
+		  </p>	
           <h2>KELEBIHAN:</h2>
           <ul>
-            <li>Bebas Iklan</li>
-            <li>Bebas Gonta-ganti lagu</li>
-            <li>Bebas download lagu dengan kualitas tertinggi</li>  
-          <h2>BERGARANSI!</h2>
-          <ul>
-            <li>Garansi 6 bulan, jika premium hilang hubungi saya!</li>
+            <li>Tanpa Iklan</li>
+            <li>Bebas gonta-ganti lagu</li>
+            <li>Download semua lagu tanpa batasan</li>
+            <li>Dengarkan musik dengan kualitas tertinggi</li>
           </ul>
+		  <h2>BERGARANSI, jika akun tiba-tiba menjadi free, bisa langsung hubungi kami!</h2>			
+		  <ul>
+            <li>
+              Garansi hanya berlaku jika akun tiba-tiba menjadi free dalam pemakaian biasa, 
+			  tidak menggunakan akun untuk perbuatan aneh-aneh!
+            </li>
+		  </ul>		            
           <p>
             Pemesanan bisa langsung hubungi kontak di halaman ini! Pembayaran via
             GoPay/OVO/Pulsa Tsel
