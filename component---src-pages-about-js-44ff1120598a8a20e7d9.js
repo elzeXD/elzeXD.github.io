3d9c670ddc7a4f544d0f9ae8d1e7dd1a31de8709
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-07-11 04:50:24 +0000

    Deploy elzeXD/elzeXD.github.io to github.com/elzeXD/elzeXD.github.io.git:master

--- component---src-pages-about-js-44ff1120598a8a20e7d9.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[8],{"3XHS":function(e,t,n){"use strict";n.r(t);var a=n("q1tI"),l=n.n(a),u=n("Bl7J"),r=n("vrFN");t.default=function(){return l.a.createElement(u.a,null,l.a.createElement(r.a,{title:"About"}),l.a.createElement("h1",null,"About"))}}}]);
-//# sourceMappingURL=component---src-pages-about-js-44ff1120598a8a20e7d9.js.map
